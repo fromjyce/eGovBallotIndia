48e56dfe5dac9f3b8fc0142731539568dcb04adb
Shanmathi Ganesan <shanmathi18g@gmail.com>
2024-01-28 13:19:00 +0530

    #

--- e-gov-ballot/src/App.js
+++ e-gov-ballot/src/App.js
@@ -1,15 +1,19 @@
-import Login from "./components/Login-Cast.js";
+// import LoginCast from "./components/Login-Cast.js";
+// import LoginCheck from "./components/Login-Check.js";
+// import LoginAdmin from "./components/Login-Admin.js";
 // import CastVote from "./components/CastVote.js";
 // import Logout from "./components/Logout.js";
-
-import React, { useEffect } from "react";
+import Dashboard from "./components/DashboardCLR.js";
 
 function App() {
   return (
     <>
-    <Login />
+      {/* <LoginCast /> */}
+      {/* <LoginCheck /> */}
+      {/* <LoginAdmin /> */}
       {/* <CastVote /> */}
       {/* <Logout /> */}
+      <Dashboard />
     </>
   );
 }
--- e-gov-ballot/src/components/DashboardCLR.js
+++ e-gov-ballot/src/components/DashboardCLR.js
@@ -1,15 +1,38 @@
 import "../styles/DashboardCLR.css";
 import HeaderLogo from "./Header-Logo.js";
-
+import React, { useEffect } from "react";
+//import Left from "./Left.js";
+import Language from "./Language.js";
+import result from "../assets/results.png";
 
 function DashboardCLR() {
-    return (
-        <div className="DashboardCLR" id="DashboardCLR">
-            <div className="dashboard-check-live-results">
-               <HeaderLogo/>
+  useEffect(() => {
+    return () => {
+      document.title = "Login";
+    };
+  }, []);
+  return (
+    <div className="DashboardCLR" id="DashboardCLR">
+        <HeaderLogo className="header-js"/>
+      <div className="dashboard-check-live-results">
+        <div className="left-side-dashboard">
+            <div className="menu-list">
+                <div className="menu-list-item one">
+                    Home
+                </div>
+                <div className="menu-list-item two">
+                    Profile
+                </div>
+                <div className="menu-list-item three">
+                    Logout
+                </div>
+                <Language />
             </div>
         </div>
-    );
+            <img src={result} className="result-image" alt="result" width={787} height={437}/>
+          </div>
+      </div>
+  );
 }
 
 export default DashboardCLR;
