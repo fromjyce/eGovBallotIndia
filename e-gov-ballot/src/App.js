5d3ebfc7fe3ce0b032c94c5fc19c24b8f83ce0d3
jaya2 <jaya2004kra@gmail.com>
2024-01-28 11:41:25 +0530

    #

--- e-gov-ballot/src/App.js
+++ e-gov-ballot/src/App.js
@@ -1,16 +1,13 @@
+import DashboardCLR from "./components/DashboardCLR.js";
 import Login from "./components/Login-Cast.js";
 // import CastVote from "./components/CastVote.js";
-// import Logout from "./components/Logout.js";
+ import Logout from "./components/Logout.js";
 
 import React, { useEffect } from "react";
 
 function App() {
   return (
-    <>
-    <Login />
-      {/* <CastVote /> */}
-      {/* <Logout /> */}
-    </>
+    <Logout/>
   );
 }
 
